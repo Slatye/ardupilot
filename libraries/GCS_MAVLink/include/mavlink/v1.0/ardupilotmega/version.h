e6ee51cc63f5d0998ad79a5b72ac36521890765e
Evan Slatyer <evan.slatyer@gmail.com>
2014-11-26 14:10:08 +1100

    Rebased to master (20141126) and rebuilt MAVLink

--- libraries/GCS_MAVLink/include/mavlink/v1.0/ardupilotmega/version.h
+++ libraries/GCS_MAVLink/include/mavlink/v1.0/ardupilotmega/version.h
@@ -1,11 +1,11 @@
 /** @file
  *	@brief MAVLink comm protocol built from ardupilotmega.xml
- *	@see http://mavlink.org
+ *	@see http://pixhawk.ethz.ch/software/mavlink
  */
 #ifndef MAVLINK_VERSION_H
 #define MAVLINK_VERSION_H
 
-#define MAVLINK_BUILD_DATE "Tue Nov 25 10:42:23 2014"
+#define MAVLINK_BUILD_DATE "Wed Nov 26 14:09:56 2014"
 #define MAVLINK_WIRE_PROTOCOL_VERSION "1.0"
 #define MAVLINK_MAX_DIALECT_PAYLOAD_SIZE 255
  
